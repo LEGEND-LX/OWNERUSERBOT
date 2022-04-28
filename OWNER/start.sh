263cdc871c4c4228b0b5261b6fafd780a35eda79
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-04-28 08:35:43 -0700

    Add files via upload

--- OWNER/start.sh
+++ OWNER/start.sh
@@ -1,1 +1,15 @@
 
+
+echo """
+ 
+╭━━╮╭━━━┳━━━━╮
+┃╭╮┃┃╭━╮┃╭╮╭╮┃
+┃╰╯╰┫┃╱┃┣╯┃┃╰╯
+┃╭━╮┃┃╱┃┃╱┃┃
+┃╰━╯┃╰━╯┃╱┃┃
+╰━━━┻━━━╯╱╰╯
+"""
+rm -rf PRO-DRAGONROBOOT
+git clone https://github.com/DRAK-LEGEND-PRO/PRO-DRAGONROBOOT
+cd PRO-USERBOT
+python3 -m userbot
