7b3b9ac44c44c3c9897920d22933826a27a47c3d
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-05-03 09:44:41 -0700

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -88,7 +88,7 @@ if ENV:
         LYDIA_API = os.environ.get("LYDIA_API", None)
         PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None))
         UPSTREAM_REPO = os.environ.get(
-            "UPSTREAM_REPO", "https://github.com/DRAK-LEGEND-PRO/PRO-DRAGONSETUP"
+            "UPSTREAM_REPO", "https://github.com/LEGEND-LX/OWNERBOT-DEPLOY"
         )
         APP_ID = os.environ.get("APP_ID", None)
         API_HASH = os.environ.get("API_HASH", None)
