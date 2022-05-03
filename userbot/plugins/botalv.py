9dcdfa74fb07781603454caa73e9084ebbd2aec3
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-05-03 09:43:41 -0700

    Add files via upload

---
+++ userbot/plugins/botalv.py
@@ -0,0 +1,61 @@
+from telethon import version
+
+from userbot import *
+from userbot.cmdhelp import CmdHelp
+from userbot.utils import *
+
+# -------------------------------------------------------------------------------
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND"
+
+ludosudo = Config.SUDO_USERS
+
+if ludosudo:
+    sudou = "True"
+else:
+    sudou = "False"
+
+legend = bot.uid
+
+mention = f"[{DEFAULTUSER}](tg://user?id={legend})"
+
+
+PM_IMG = "https://te.legra.ph/file/fef2d78da4110fbf98a2e.jpg"
+pm_caption = "**DARGONROẞOO✞︎ 𝙸𝚜 𝙾𝚗𝚕𝚒𝚗𝚎**\n\n"
+
+pm_caption += f"**┏🔥✞t͛ẞ̸DragonRoẞoo✞︎t🔥┓**\n"
+pm_caption += f"**┣.ۗۗ𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𓄂 𝙼𝚢 𝙼𝚊𝚜𝚝𝚎𝚛    : {mention}**\n"
+pm_caption += f"**┣.ۗۗ𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𓄂 𝚃𝚎𝚕𝚎𝚝𝚑𝚘𝚗 : `{version.__version__}`**\n"
+pm_caption += f"**┣.ۗۗ𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𓄂DragonRoẞoo✞︎ : {LEGENDversion}**\n"
+pm_caption += f"**┣.ۗۗ𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𓄂 𝚂𝚞𝚍𝚘     : `{sudou}`**\n"
+pm_caption += f"**┣.ۗۗ𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𑲭𓄂 𝙾𝚠𝚗𝚎𝚛     : [𝖑𝖊ɠêɳ̃D](https://t.me/DARK_LEGEND_PRO)**\n"
+pm_caption += f"**┗[♦️𝙶𝚛𝚘𝚞𝚙♦️](https://t.me/DragonPro_Userbot)┛**\n"
+
+pm_caption += "┏[𝖑𝖊ɠêɳ̃D ~ Pro](github.com/DARK-LEGEND-PRO)┓"
+
+
+@bot.on(admin_cmd(outgoing=True, pattern="bot$"))
+@bot.on(sudo_cmd(pattern="bot$", allow_sudo=True))
+async def amireallyalive(alive):
+    if alive.fwd_from:
+        return
+    await alive.get_chat()
+    await alive.delete()
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
+    await alive.delete()
+
+
+CmdHelp("alv").add_command(
+    "alive", None, "Check weather the bot is alive or not"
+).add_command(
+    "bot",
+    None,
+    "Check weather the bot is alive or not. In your custom Alive Pic and Alive Msg",
+).add_warning(
+    "Harmless Module"
+).add_info(
+    "Are u alive?"
+).add_type(
+    "Official"
+).add()
